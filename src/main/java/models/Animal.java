bd4eb63c4808d784d14e5c1996632edf7173369b
Abel <abelberhane011@gmail.com>
2019-11-22 10:44:10 +0300

    first commit, adds gitignore

---
+++ src/main/java/models/Animal.java
@@ -0,0 +1,23 @@
+package models;
+
+public class Animal {
+
+    private String name;
+    private int id;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
---
+++ src/test/java/models/AnimalTest.java
@@ -0,0 +1,18 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class AnimalTest {
+
+    @Before
+    public void setUp() throws Exception {
+        Animal animal = new Animal();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
