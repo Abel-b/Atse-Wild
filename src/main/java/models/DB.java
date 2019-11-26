11c782a0bf3e6449983092ca457be39e58d8bd9c
Abel <abelberhane011@gmail.com>
2019-11-26 11:53:02 +0300

    adds heroku's db structure

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -1,7 +1,29 @@
 package models;
 import org.sql2o.*;
-
-
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "moringa", "berhane1234");
+    private static URI dbUri;
+    public static Sql2o sql2o;
+    static Logger logger = LoggerFactory.getLogger(DB.class);
+    static {
+
+        try {
+            if (System.getenv("DATABASE_URL") == null) {
+                dbUri = new URI("postgres://localhost:5432/wildlife_tracker");
+            } else {
+                dbUri = new URI(System.getenv("DATABASE_URL"));
+            }
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo() == null) ? "moringa" : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == null) ? "berhane1234" : dbUri.getUserInfo().split(":")[1];
+            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+        } catch (URISyntaxException e ) {
+            logger.error("Unable to connect to database.");
+        }
+    }
 }
